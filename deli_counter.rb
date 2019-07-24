5ed2ceab27a5b5d18a49e6a0908d0bda09dab592
Andrew Rosario <rosario.andrew@gmail.com>
2019-07-24 14:59:52 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,10 +4,10 @@ def line(line)
   if line.length == 0 
     puts "The line is currently empty."
   else
-    line_string = "The line is currently: "
+    line_string = "The line is currently:"
     i = 1
     line.each do |name| 
-      line_string += "#{i}. #{name} "
+      line_string += " #{i}. #{name}"
       i += 1 
     end
     puts line_string
