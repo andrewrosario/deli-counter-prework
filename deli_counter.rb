0b5e58d8bb53dc23285e1b155b141ebaa9fc8c0a
Andrew Rosario <rosario.andrew@gmail.com>
2019-07-24 15:05:04 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -18,3 +18,10 @@ def take_a_number(line, name)
   line << name
   puts "Welcome, #{name}. You are number #{line.length} in line."
 end
+
+def now_serving(line)
+  if line.length == 0
+    puts "The line is currently empty."
+  else
+    puts "Currently serving #{line.shift}."
+end
